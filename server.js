dae0eeba3c37c9f7f83253f854ca8b515bf298ba
Ismael Meza <todoesculpadelgobierno@gmail.com>
2019-06-17 12:24:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist/ng'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ng/index.html'));
 });
 
 app.set('port', process.env.PORT || 8080);
