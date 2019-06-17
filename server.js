2bc5971a57f294a41bfe0bc46ca92009154d4bbc
Ismael Meza <todoesculpadelgobierno@gmail.com>
2019-06-17 12:08:53 -0500

    Algo que se me pasó uwu

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get('/*', (req, res) => {
 app.set('port', process.env.PORT || 8080);
 
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${this.app.get('port')}`);
+    console.log(`Server on port ${app.get('port')}`);
 });
